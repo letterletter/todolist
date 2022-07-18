d54386ae04ee8537c2155639630ddca52f1e7ca5
wufan12 <wufan12@kuaishou.com>
2022-07-18 14:05:41 +0800

    feat: edit todolist

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ let currentPage = 'index';
 let checkAll = false;
 let todoList = [
 ];
+let editId = ''
+
 
 app.use(bodyParser());
 app.use(views(path.join(__dirname, './views'), {
@@ -19,33 +21,38 @@ app.use(views(path.join(__dirname, './views'), {
 
 app.use(static(path.join(__dirname, '/public')))
 
-
 router.get('/', async (ctx, next) => {
+
   currentPage = 'index';
-  await ctx.render('index', { dataList: todoList, unfinished, pathname: '/', checkAll })
+  await ctx.render('index', { dataList: todoList, unfinished, pathname: '/', checkAll, editId })
 })
 
 router.get('/active', async (ctx) => {
+
   currentPage = 'active';
   await ctx.render('index', {
     dataList: todoList.filter(item => item.status === 'active'),
     unfinished,
     pathname: ctx.request.url,
-    checkAll
+    checkAll,
+    editId
   });
 })
 
 router.get('/complete', async (ctx) => {
+
   currentPage = 'complete';
   await ctx.render('index', {
     dataList: todoList.filter(item => item.status !== 'active'),
     unfinished,
     pathname: ctx.request.url,
-    checkAll
+    checkAll,
+    editId
   });
 })
 
 router.post('/save', async (ctx) => {
+
   const { title } = ctx.request.body;
   if(!title) return;
   todoList.push({ id: Math.random().toString(36).slice(6), title, status: 'active' });
@@ -56,6 +63,7 @@ router.post('/save', async (ctx) => {
 })
 
 router.get('/changeStatus/:id', async (ctx) => {
+
   let { id } = ctx.params;
   let findItem = todoList.find(item => item.id === id);
   if(!findItem) return;
@@ -68,8 +76,9 @@ router.get('/changeStatus/:id', async (ctx) => {
 })
 
 router.get('/checkall', async (ctx) => {
+
   checkAll = !checkAll;
-  changeTodoList(checkAll, currentPage)
+  changeTodoList(checkAll)
   await ctx.redirect(`/${currentPage.replace('index', '')}`);
 })
 
@@ -85,12 +94,30 @@ router.post('/delete', async (ctx) => {
   }
 })
 
+router.get('/edit/:id', async ctx => {
+  let { id } = ctx.params;
+  editId = id;
+  await ctx.redirect(`/${currentPage.replace('index', '')}`);
+})
+
+router.post('/saveEdit/:id', async ctx => {
+  const { id } = ctx.params;
+  const { title } = ctx.request.body;
+  editId = ''
+  for (let index = 0, size = todoList.length; index < size; index++) {
+    if(todoList[index].id === id) {
+      todoList[index].title = title;
+      break;
+    }
+  }
+  await ctx.redirect(`/${currentPage.replace('index', '')}`);
+})
 
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000);
 
-function changeTodoList(checked, currentPage) {
+function changeTodoList(checked) {
   let status = checked ? 'complete' : 'active';
   unfinished = checked ? 0 : todoList.length;
   todoList.forEach(element => {
